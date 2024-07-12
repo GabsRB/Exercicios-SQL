05100804c8ba880d892892f15c46fe717984ffb2
Gabrielle Barreto <105834363+GabsRB@users.noreply.github.com>
2024-07-12 18:12:42 -0300

    Add files via upload

---
+++ Mundo.sqlite.sql
@@ -0,0 +1,27 @@
+-- TABLE
+CREATE TABLE "city" (
+  "name_city" varchar(35),
+  "country_city" varchar(35),
+  "population_city" real,
+  "capital_city" varchar(1)
+  );
+CREATE TABLE "country" (
+  "name_country" varchar(35),
+  "continete_country" varchar(35),
+  "population_country" real,
+  "pib" real,
+  "expec_vida" real
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE "rio" (
+  "name_rio" varchar(35),
+  "origem_rio" varchar(35),
+  "comprimento_rio" integer
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
