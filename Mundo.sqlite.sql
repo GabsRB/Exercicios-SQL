53a43d490ae889d4dd59dc252aad7ef0598e31c3
Gabrielle Barreto <105834363+GabsRB@users.noreply.github.com>
2024-07-12 18:41:44 -0300

    Add files via upload

--- Mundo.sqlite.sql
+++ Mundo.sqlite.sql
@@ -1,11 +1,13 @@
 -- TABLE
 CREATE TABLE "city" (
+  "id_city" integer PRIMARY key AUTOINCREMENT,
   "name_city" varchar(35),
   "country_city" varchar(35),
   "population_city" real,
   "capital_city" varchar(1)
   );
 CREATE TABLE "country" (
+  "id_country" integer PRIMARY key AUTOINCREMENT,
   "name_country" varchar(35),
   "continete_country" varchar(35),
   "population_country" real,
@@ -14,6 +16,7 @@ CREATE TABLE "country" (
   );
 CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
 CREATE TABLE "rio" (
+  "id_rio" integer PRIMARY key AUTOINCREMENT,
   "name_rio" varchar(35),
   "origem_rio" varchar(35),
   "comprimento_rio" integer
